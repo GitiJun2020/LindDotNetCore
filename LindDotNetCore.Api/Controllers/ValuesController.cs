db98dc7a68d364ef4e4f4ed3b2ebe88df3d56c4a
zhangzl <zhangzl@i-counting.cn>
2019-03-13 14:11:35 +0800

    添加setting.json

--- LindDotNetCore.Api/Controllers/ValuesController.cs
+++ LindDotNetCore.Api/Controllers/ValuesController.cs
@@ -1,33 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-namespace LindDotNetCore.Api.Controllers {
-	[Route ("api/[controller]")]
-	public class ValuesController : Controller {
+﻿using System; 
+using System.Collections.Generic; 
+using System.Linq; 
+using System.Threading.Tasks; 
+using Microsoft.AspNetCore.Mvc; 
+using LindDotNetCore.Logger; 
+namespace LindDotNetCore.Api.Controllers 
+{
+	[Route ("api/[controller]")]	
+	public class ValuesController:Controller 
+    {
+		ILogger logger; 
+		public ValuesController (ILogger logger)
+        {
+			this.logger = logger; 
+		}
 		// GET api/values
 		[HttpGet]
-		public IEnumerable<string> Get () {
-			return new string[] { "value1", "value2" };
+		public IEnumerable < string > Get ()
+        {
+			logger.Debug ("hello world api value"); 
+			return new string[] 
+            {"value1", "value2"}; 
 		}
 
 		// GET api/values/5
 		[HttpGet ("{id}")]
-		public string Get (int id) {
-			return "value";
+		public string Get (int id)
+        {
+			return "value"; 
 		}
 
 		// POST api/values
 		[HttpPost]
-		public void Post ([FromBody] string value) { }
+		public void Post ([FromBody] string value)
+        {}
 
 		// PUT api/values/5
 		[HttpPut ("{id}")]
-		public void Put (int id, [FromBody] string value) { }
+		public void Put (int id, [FromBody] string value)
+        {}
 
 		// DELETE api/values/5
 		[HttpDelete ("{id}")]
-		public void Delete (int id) { }
+		public void Delete (int id)
+        {}
 	}
 }
