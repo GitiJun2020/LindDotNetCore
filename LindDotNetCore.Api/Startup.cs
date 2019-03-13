7381323eaca0f894074efa729f95c4e522523d2d
zhangzl <zhangzl@i-counting.cn>
2019-03-13 14:25:35 +0800

    添加了仓储引用

--- LindDotNetCore.Api/Startup.cs
+++ LindDotNetCore.Api/Startup.cs
@@ -1,34 +1,46 @@
-﻿using System; 
-using System.Collections.Generic; 
-using System.Linq; 
-using System.Threading.Tasks; 
-using Microsoft.AspNetCore.Builder; 
-using Microsoft.AspNetCore.Hosting; 
-using Microsoft.Extensions.Configuration; 
-using Microsoft.Extensions.DependencyInjection; 
-using Microsoft.Extensions.Logging; 
-using Microsoft.Extensions.Options; 
-namespace LindDotNetCore.Api {
-public class Startup {  
-public Startup (IConfiguration configuration) {
-Configuration = configuration; 
-}
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LindDotNetCore.Repository;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+namespace LindDotNetCore.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
 
-public IConfiguration Configuration {get; }
+        public IConfiguration Configuration
+        { get; }
 
-// This method gets called by the runtime. Use this method to add services to the container.
-public void ConfigureServices (IServiceCollection services) {
-services.AddLoggerConsole(); 
-services.AddMvc (); 
-}
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddLoggerConsole();
+            services.AddEf((o) =>
+            {
+                o.DbType = DbType.MySql;
+            });
+            services.AddMvc();
+        }
 
-// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-public void Configure (IApplicationBuilder app, IHostingEnvironment env) {
-if (env.IsDevelopment ()) {
-app.UseDeveloperExceptionPage (); 
-}
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
 
-app.UseMvc (); 
-}
-}
+            app.UseMvc();
+        }
+    }
 }
